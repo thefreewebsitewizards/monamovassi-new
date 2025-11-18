05fbb60ea89e65be0c4a01dbffde29418739a4a8
The Free Website Wizards <dylan@thefreewebsitewizards.com>
2025-11-18 22:19:54 +0800

    Revised

--- config/emailjs-config.js
+++ config/emailjs-config.js
@@ -1,4 +1,4 @@
-const EMAILJS_PUBLIC_KEY  = "";                 
+const EMAILJS_PUBLIC_KEY  = "83ucnPhbgMXHULiXL";                 
 const EMAILJS_SERVICE_ID  = "service_oxw6pmq";  
 const EMAILJS_TEMPLATE_ID = "template_b8xybo8"; 
 
